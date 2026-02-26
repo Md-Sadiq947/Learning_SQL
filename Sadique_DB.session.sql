803b3020dcd7ba704bd9b1874c6ac577975e5865
Md-Sadiq947 <mdsadique822499@gmail.com>
2026-02-26 18:01:32 +0000

    Using aggregate functions

---
+++ Sadique_DB.session.sql
@@ -0,0 +1,34 @@
+INSERT into person(first_name, last_name, gender, date_of_birth, email, country_of_birth)
+VALUES('syed', 'saad', 'male', '2011-09-19', 'hamza@gmail.com', 'germany'); 
+
+--SELECT * FROM person;
+--SELECT * FROM person ORDER BY country_of_birth; 
+
+SELECT * FROM person WHERE gender = 'Male' AND (country_of_birth = 'Indonesia' OR country_of_birth = 'Germany' OR gender = 'Non-binary');
+
+-- SELECT * FROM person OFFSET 10 FETCH FIRST ROW ONLY; 
+
+SELECT * FROM person WHERE country_of_birth = 'Germany' OR country_of_birth = 'France' OR country_of_birth = 'Indonesia';
+SELECT * FROM person WHERE country_of_birth IN ('Germany', 'France', 'Indonesia') ORDER BY country_of_birth; -- IN operator takes array
+
+SELECT * FROM person WHERE email LIKE '%.com';
+SELECT * FROM person WHERE country_of_birth ILIKE 'G%';
+
+SELECT DISTINCT country_of_birth FROM person;
+SELECT DISTINCT country_of_birth FROM person GROUP BY country_of_birth;
+SELECT DISTINCT country_of_birth, COUNT(*)  FROM person GROUP BY country_of_birth;
+SELECT DISTINCT country_of_birth, COUNT(*)  FROM person GROUP BY country_of_birth ORDER BY country_of_birth;
+
+SELECT * FROM car;
+
+SELECT AVG(price) FROM car;
+--using aggregate functions 
+
+SELECT ROUND(AVG(price)) FROM car;
+
+
+SELECT ROUND(AVG(price)) FROM car;
+
+SELECT make, MAX(price) FROM car GROUP BY make; SELECT make, MIN(price) FROM car GROUP BY make; SELECT make, AVG(price) FROM car GROUP BY make;
+
+
